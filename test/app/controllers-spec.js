0e941f92be6985c7a6b542331990fcf8cc43e6dd
Jeff Heinrichs <jeff.heinrichs@icfi.com>
2015-06-24 15:41:02 -0500

    fixes for new api call

--- test/app/controllers-spec.js
+++ test/app/controllers-spec.js
@@ -7,14 +7,14 @@ describe("Ads18fController", function () {
         httpBackend,
         DATA_PATHS,
         foundResponse = {
-            "found"          : "true",
+            "found"          : true,
             "brand_name"     : "oxycodone",
             "generic_name"   : "oxycodone",
             "substance_name" : "oxycodone",
             "spl_medguide"   : "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare, arcu in molestie ornare, tellus dui consequat urna, in cursus tortor libero at elit. Integer id diam sit amet mauris vulputate ornare. Praesent vel dignissim ligula. Nunc erat orci, volutpat sagittis vehicula ac, venenatis et neque. Curabitur consectetur in nisi vel placerat. Nam a risus lectus. Aliquam condimentum diam et erat dictum condimentum. Nam ornare turpis id pretium rhoncus.</p><p><p>Nam efficitur <span class=\"highlight\">oxycodone</span> quis posuere tristique. Vestibulum turpis ante, accumsan eu purus eget, posuere tempor dolor. Fusce sed fringilla enim. Sed tempus tristique varius. Vivamus cursus sed tortor et mattis. Vivamus varius finibus odio condimentum pulvinar. Phasellus vel arcu tincidunt, bibendum erat eu, sodales neque. Fusce rutrum iaculis dolor quis tincidunt.</p><p>Sed facilisis, enim ac convallis rhoncus, elit diam iaculis velit, eget tristique augue enim quis mauris. Fusce sed vehicula diam. Vestibulum enim metus, sodales eget aliquet eu, dapibus id erat. Ut tempor est sed pretium interdum. Nullam vel finibus orci. Etiam eget volutpat orci. Donec vel mauris vel massa eleifend dictum a in neque.</p>"
         },
         notFoundResponse = {
-            "found"          : "false",
+            "found"          : false,
             "brand_name"     : "oxycodone",
             "generic_name"   : "oxycodone",
             "substance_name" : "oxycodone",
